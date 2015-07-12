1e4cc47fa52b8fa47577e6608c158bf1efc70ec1
Tiddo Langerak <tiddolangerak@gmail.com>
2015-07-12 18:13:04 +0200

    Made more robust \& added support for only triggering once

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ export default angular.module('mm.onEnterViewport', [])
 	 * @ngdoc directive
 	 * @name mm.onEnterViewport.mmOnEnterViewport
 	 * @description
-	 * Detects when an element enters the viewport
+	 * Detects when an element enters the viewport and triggers a callback as soon as it happens. Note
+	 * that if an element disappears from the viewport and later reappears it will, by default, trigger
+	 * its callback again. This can be disabled with the `triggerOnce` attribute.
 	 *
 	 * @restrict A
 	 * @param {&Function} mmOnEnterViewport Callback that will be triggered when the element enters the viewport
@@ -21,6 +23,9 @@ export default angular.module('mm.onEnterViewport', [])
 	 *                                  is in the upper 70% of the screen. Note that setting a too high value here
 	 *                                  will prevent elements at the bottom of the page to ever trigger the event.
 	 *                                  This value can also be configured globally with the mmOnEnterViewportProvider (see example code)
+	 * @param {@Bool} [triggerOnce] If set to true then the callback will only be called when the element
+	 *                              enters the viewport for the first time. Later appearances will not
+	 *                              trigger the callback again.
 	 *
 	 * @example
 	 * <example module="on-enter-viewport-demo">
@@ -48,23 +53,33 @@ export default angular.module('mm.onEnterViewport', [])
 	.directive('mmOnEnterViewport', ($timeout, $parse, mmOnEnterViewport, $rootScope) => {
 
 		function isInView(element, thresholdBottom) {
+			if (!element.getBoundingClientRect) {
+				return false;
+			}
 			const boundingRect = element.getBoundingClientRect();
 			return boundingRect.height > 0 && boundingRect.width > 0 && boundingRect.bottom > 0 && boundingRect.top < window.innerHeight * (1 - thresholdBottom);
 		}
 
 		return {
 			restrict : 'A',
+			//We want any other directive on elements to be compiled before mm-on-enter-viewport is attached
+			//therefore, negative priority
+			priority : -1,
 			link : function (scope, elem, attrs) {
 				const callbackGetter = $parse(attrs.mmOnEnterViewport);
 				const thresholdBottom = attrs.thresholdBottom ? $parse(attrs.thresholdBottom)(scope) : mmOnEnterViewport.thresholdBottom;
 				const viewportWindowEvents = ['scroll', 'resize'];
 
 				let visible = false;
+				let triggered = false;
+
 				function checkVisibility() {
 					const currentlyVisible = isInView(elem[0], thresholdBottom);
 					if (currentlyVisible !== visible) {
 						visible = currentlyVisible;
-						if (visible) {
+						const triggerOnce = attrs.triggerOnce === 'true';
+						if (visible && (!triggerOnce || !triggered)) {
+							triggered = true;
 							scope.$apply(function() {
 								callbackGetter(scope);
 							});
